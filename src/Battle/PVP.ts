135cab746f36c78b88b48a3c0c31626fbe644515
Thales <thalessilva.tv@gmail.com>
2022-04-20 10:19:42 -0300

    req 10: pvp class implements

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,17 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+export default class PVP extends Battle {
+  constructor(protected player1: Fighter, protected player2: Fighter) {
+    super(player1);
+  }
+
+  fight(): number {
+    while (this.player1.lifePoints > 0 || this.player2.lifePoints > 0) {
+      this.player1.attack(this.player2);
+      if (this.player2.lifePoints > 0) this.player2.attack(this.player1);
+    }
+
+    return this.player1.lifePoints === -1 ? -1 : 1;
+  }
+}
